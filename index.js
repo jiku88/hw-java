d78d6dbe3fd1e51b0ca56f6dbe2417faf53d1a12
jiku88 <jutirmoydatta73@gmail.com>
2023-10-09 18:21:05 +0600

    number one done

---
+++ index.js
@@ -0,0 +1,43 @@
+// 2 values addtion
+
+function addTwoNumber(a,b){
+    var a;
+    var b;
+    var c = a + b;
+
+    return c;
+}
+console.log(addTwoNumber(5,10));
+
+// 2 values multiplication
+
+function maltiTwoNumber(d,e){
+    var d;
+    var e;
+    var f = d * e;
+
+    return f;
+}
+console.log(maltiTwoNumber(5,6));
+
+// 2 values substraction
+
+function subTwoNumber(a,b){
+    var a;
+    var b;
+    var c = a - b;
+
+    return c;
+}
+console.log(subTwoNumber(30,5));
+
+// 2 values divition
+
+function diviTwoNumber(a,b){
+    var a;
+    var b;
+    var c = a / b;
+
+    return c;
+}
+console.log(diviTwoNumber(50,2));
