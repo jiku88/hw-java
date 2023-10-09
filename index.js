55fa4f5cb80084d561eaf4db0fc6ca458ccd594d
jiku88 <jutirmoydatta73@gmail.com>
2023-10-09 19:59:09 +0600

    number three done

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Number one
 // 2 values addtion
 
 function addTwoNumber(a,b){
@@ -42,9 +43,32 @@ function diviTwoNumber(a,b){
 }
 console.log(diviTwoNumber(50,2));
 
-
+// Number Two
 // convert a string into uppercase
 
 var name = "My name is Juttirmoy datta Jiku"
 
 console.log(name.toUpperCase())
+
+// Number Three
+// student grade sheet
+
+var marks = 60;
+
+if(marks >= 80){
+    console.log("A+")
+}else if(marks >= 75){
+    console.log("A")
+}else if(marks >= 70){
+    console.log("A-")
+}else if(marks >= 60 & 65){
+    console.log("B+")
+}else if(marks > 50){
+    console.log("B")
+}else if(marks > 40){
+    console.log("C")
+}
+
+else{
+    console.log("Fail")
+}
